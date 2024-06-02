0b2bc9d44ae20f98255075a44b891a5125035b75
DevMousavi <mousavii.dev>
2024-06-02 11:25:16 +0330

    The "updateAccessToken" function was written to receive the new AccessToken

--- src/Pages/Auth/SingUp.jsx
+++ src/Pages/Auth/SingUp.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { updateAccessToken } from "../../services/updateAccessToken";
 
 const SingUp = () => {
+    updateAccessToken("b866b6a9-d61e-498e-97bf-fce5431ac5ab");
     return <div>SingUp</div>;
 };
 
--- src/services/Config.js
+++ src/services/Config.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: import.meta.VITE_BASE_URL });
+const api = axios.create({ baseURL: import.meta.env.VITE_BASE_URL });
 
 export { api };
