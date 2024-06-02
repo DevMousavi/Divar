0eca64d79afe5705d7958483a43fee79fb1199c7
DevMousavi <mousavii.dev>
2024-06-02 10:44:14 +0330

    The cookie settings function was written

---
+++ src/Utils/setCookies.js
@@ -0,0 +1,15 @@
+const setCookies = (tokenName, tokenValue) => {
+    if (tokenName === "accessToken") {
+        document.cookie = `${tokenName}=${tokenValue} ;max-age=${44}`;
+    } else if (tokenName === "refreshToken") {
+        document.cookie = `${tokenName}=${tokenValue} ;max-age=${
+            30 * 24 * 60 * 60
+        }`;
+    } else {
+        return {
+            message: `No Commands Have Been Placed For This ${tokenName}`,
+        };
+    }
+};
+
+export { setCookies };
