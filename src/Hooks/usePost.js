366ccb7c659a596fcf9e5dc4b010507985887cfd
DevMousavi <mousavii.dev>
2024-06-03 14:21:09 +0330

    The "useMutation" hook in React Query has been set up for "Login.jsx".

--- src/Hooks/usePost.js
+++ src/Hooks/usePost.js
@@ -1,9 +1,9 @@
 import { api } from "../services/Config";
 
 // A function to send requests with the "GET" method
-const usePostData = async (url) => {
+const usePostData = async (url, data) => {
     try {
-        const request = api.get(url);
+        const request = api.post(url, data);
         return request;
     } catch (error) {
         return error;
--- src/Pages/Auth/Login.jsx
+++ src/Pages/Auth/Login.jsx
@@ -1,18 +1,45 @@
 import React, { useRef } from "react";
 import HeaderAuth from "./HeaderAuth";
+import { useMutation } from "@tanstack/react-query";
+import { api } from "../../services/Config";
 
 const Login = () => {
     const phoneNumber = useRef(null);
     const passwordUser = useRef(null);
 
+    const mutation = useMutation({
+        mutationFn: async (userInformation) => {
+            const req = await api.post("/auth/login", userInformation);
+            return req;
+        },
+        onSuccess: (res) => {
+            console.log(res);
+        },
+        onError: (err) => {
+            console.log(err);
+        },
+    });
+
     const submitHandler = (even) => {
         even.preventDefault();
-        console.log(even);
 
-        const data = {
+        const userInformation = {
             phone: phoneNumber.current.value,
             password: passwordUser.current.value,
         };
+
+        mutation.mutate({
+            phone: "09131211512",
+            password: "123456789",
+        });
+
+        console.log({
+            data: mutation.data,
+            isError: mutation.isError,
+            error: mutation.error,
+            isLoading: mutation.isLoading,
+            isFetching: mutation.isFetching,
+        });
     };
 
     return (
